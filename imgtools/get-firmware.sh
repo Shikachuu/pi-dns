ad25882dd8ce58589abcab210b484a5848413e38
Czékus Máté <mate@picloud.hu>
2024-08-25 19:13:30 +0200

    feat: move firmware downloads from the container to local machine
    Signed-off-by: Czékus Máté <mate@picloud.hu>

--- imgtools/get-firmware.sh
+++ imgtools/get-firmware.sh
@@ -3,4 +3,4 @@
 mkdir -p /tmp/RPi4boot/boot/efi/
 sudo dnf install -y --downloadonly --release=40 --forcearch=aarch64 --destdir=/tmp/RPi4boot/ uboot-images-armv8 bcm283x-firmware bcm283x-overlays
 for rpm in /tmp/RPi4boot/*rpm; do rpm2cpio $rpm | sudo cpio -idv -D /tmp/RPi4boot/; done
-sudo mv /tmp/RPi4boot/usr/share/uboot/rpi_arm64/u-boot.bin /tmp/RPi4boot/rpi-u-boot.bin
+sudo mv /tmp/RPi4boot/usr/share/uboot/rpi_arm64/u-boot.bin /tmp/RPi4boot/boot/efi/rpi-u-boot.bin
